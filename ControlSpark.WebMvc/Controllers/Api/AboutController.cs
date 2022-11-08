43b5218344afc397b1321a4b1d59850219d8cba6
Mark Hazleton <mark.hazleton@controlorigins.com>
2022-11-08 17:25:31 -0600

    updates

--- ControlSpark.WebMvc/Controllers/Api/AboutController.cs
+++ ControlSpark.WebMvc/Controllers/Api/AboutController.cs
@@ -1,16 +1,26 @@
 namespace ControlSpark.WebMvc.Controllers.Api;
 
+/// <summary>
+/// AboutController
+/// </summary>
 [Route("api/[controller]")]
 [ApiController]
 public class AboutController : ControllerBase
 {
     private readonly IAboutProvider _aboutProvider;
 
+    /// <summary>
+    /// AboutController
+    /// </summary>
+    /// <param name="aboutProvider"></param>
     public AboutController(IAboutProvider aboutProvider)
     {
         _aboutProvider = aboutProvider;
     }
 
+    /// <summary>
+    /// GetAbout
+    /// </summary>
     [HttpGet]
     public async Task<AboutModel> GetAbout()
     {
