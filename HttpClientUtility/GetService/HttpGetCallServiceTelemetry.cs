f1d3db743f686a476f4c529f031fb9fabe4c547a
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-09-06 14:08:51 -0500

    work on meta tags

--- HttpClientUtility/GetService/HttpGetCallServiceTelemetry.cs
+++ HttpClientUtility/GetService/HttpGetCallServiceTelemetry.cs
@@ -3,16 +3,31 @@ using System.Diagnostics;
 
 namespace HttpClientUtility.GetService;
 
+/// <summary>
+/// Handles telemetry for an HTTP GET call.
+/// </summary>
 public class HttpGetCallServiceTelemetry : IHttpGetCallService
 {
     private readonly ILogger<HttpGetCallServiceTelemetry> _logger;
     private readonly IHttpGetCallService _service;
 
+    /// <summary>
+    /// Constructs a new instance 
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="service"></param>
     public HttpGetCallServiceTelemetry(ILogger<HttpGetCallServiceTelemetry> logger, IHttpGetCallService service)
     {
         _logger = logger;
         _service = service;
     }
+    /// <summary>
+    /// Get Async
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="statusCall"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
     public async Task<HttpGetCallResults> GetAsync<T>(HttpGetCallResults statusCall, CancellationToken ct)
     {
         Stopwatch sw = new();
@@ -24,7 +39,7 @@ public class HttpGetCallServiceTelemetry : IHttpGetCallService
         }
         catch (Exception ex)
         {
-            _logger.LogCritical("Telemetry:GetAsync:Exception", ex.Message);
+            _logger.LogError(ex, "Error occurred in GetAsync for StatusCall.Path: {StatusPath}", statusCall.StatusPath);
         }
         sw.Stop();
         response.ElapsedMilliseconds = sw.ElapsedMilliseconds;
---
+++ WebSpark.Portal/Utilities/NavigationMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+namespace WebSpark.Portal.Utilities;
+
+
+public class MenuItem
+{
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("icon")]
+    public string Icon { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("keywords")]
+    public string Keywords { get; set; }
+}
+
+public class NavigationMenu
+{
+    [JsonPropertyName("menuItems")]
+    public List<MenuItem> MenuItems { get; set; }
+}
+
