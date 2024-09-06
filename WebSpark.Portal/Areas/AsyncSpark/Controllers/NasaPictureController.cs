10f0fcf7f9f9dbace3255350c1bdbd28ad98d5cb
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-09-05 21:36:56 -0500

    Add Nasa POD to the AsyncSpark demo

---
+++ WebSpark.Portal/Areas/AsyncSpark/Controllers/NasaPictureController.cs
@@ -0,0 +1,27 @@
+﻿using HttpClientUtility.SendService;
+using WebSpark.Portal.Areas.AsyncSpark.Models.Nasa;
+
+namespace WebSpark.Portal.Areas.AsyncSpark.Controllers;
+
+public class NasaPictureController(
+    ILogger<NasaPictureController> logger, 
+    IHttpClientSendService service) : AsyncSparkBaseController
+{
+    public async Task<IActionResult> Index(CancellationToken ct = default)
+    {
+        var apiRequest = new HttpClientSendRequest<NasaPictureListDto>
+        {
+            CacheDurationMinutes = 500,
+            RequestPath = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5"
+        };
+
+        if (service == null)
+        {
+            logger.LogError("_service is null");
+            throw new NullReferenceException(nameof(service));
+        }
+
+        apiRequest = await service.HttpClientSendAsync(apiRequest, ct).ConfigureAwait(false);
+        return View(apiRequest);
+    }
+}
---
+++ WebSpark.Portal/Areas/AsyncSpark/Models/Nasa/NasaPictureDto.cs
@@ -0,0 +1,16 @@
+﻿namespace WebSpark.Portal.Areas.AsyncSpark.Models.Nasa;
+using System.Text.Json.Serialization;
+
+public record NasaPictureDto(
+    [property: JsonPropertyName("date")] string Date,
+    [property: JsonPropertyName("explanation")] string Explanation,
+    [property: JsonPropertyName("hdurl")] string HdUrl,
+    [property: JsonPropertyName("media_type")] string MediaType,
+    [property: JsonPropertyName("service_version")] string ServiceVersion,
+    [property: JsonPropertyName("title")] string Title,
+    [property: JsonPropertyName("url")] string Url,
+    [property: JsonPropertyName("copyright")] string Copyright
+);
+public class NasaPictureListDto : List<NasaPictureDto>
+{
+}
