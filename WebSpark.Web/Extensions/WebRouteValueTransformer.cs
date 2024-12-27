ce40e2e4dee7799eb8847ab7041cf8d8c6f85d26
Mark Hazleton <mark.hazleton@controlorigins.com>
2024-12-27 11:44:45 -0600

    rework issues with routing and redirect for bad url

--- WebSpark.Web/Extensions/WebRouteValueTransformer.cs
+++ WebSpark.Web/Extensions/WebRouteValueTransformer.cs
@@ -8,131 +8,47 @@ namespace WebSpark.Web.Extensions;
 /// </summary>
 public class WebRouteValueTransformer(ILogger<WebRouteValueTransformer> logger) : DynamicRouteValueTransformer
 {
-    /// <summary>
-    /// Transforms the route values asynchronously.
-    /// </summary>
-    /// <param name="httpContext">The current HttpContext.</param>
-    /// <param name="values">The route values.</param>
-    /// <returns>A task representing the asynchronous operation.</returns>
-    public override ValueTask<RouteValueDictionary> TransformAsync(HttpContext httpContext, RouteValueDictionary values)
-    {
-        var route = httpContext?.Request?.Path.Value?.ToLower();
-        if (string.IsNullOrEmpty(route))
-        {
-            logger.LogError("Route is null or empty");
-            values["controller"] = "error";
-            values["action"] = "notfound";
-            values["id"] = string.Empty;
-            return new ValueTask<RouteValueDictionary>(values);
-        }
 
-        if (HandleFileExtensions(route, values)
-            || HandleBlogLinks(route, values)
-            || HandleAdmin(route, values))
-        {
-            return new ValueTask<RouteValueDictionary>(values);
-        }
-        return HandleBaseView(httpContext, route, values);
+    private static readonly Dictionary<string, (string Controller, string Action)> PredefinedRoutes = new()
+    {
+        { "/blog", (RouteConstants.Blog, "index") },
+        { "/admin", (RouteConstants.Admin, "index") }
+    };
+
+    private static ValueTask<RouteValueDictionary> CreateErrorRoute(RouteValueDictionary values)
+    {
+        SetValues(values, RouteConstants.Error, "notfound", string.Empty);
+        return new ValueTask<RouteValueDictionary>(values);
     }
 
-    /// <summary>
-    /// Handles the file extensions in the route.
-    /// </summary>
-    /// <param name="route">The route value.</param>
-    /// <param name="values">The route values.</param>
-    /// <returns><c>true</c> if the file extension is handled; otherwise, <c>false</c>.</returns>
-    private bool HandleFileExtensions(string route, RouteValueDictionary values)
+    private WebsiteVM? GetBaseView(HttpContext httpContext)
     {
-        if (route.EndsWith(".php"))
+        try
         {
-            SetValues(values, "EmptyFiles", "BlankPHP", string.Empty);
-            return true;
+            var sessionData = httpContext.Session.GetString(RouteConstants.BaseViewSessionKey);
+            return string.IsNullOrEmpty(sessionData) ? null : JsonSerializer.Deserialize<WebsiteVM>(sessionData);
         }
-        if (route.EndsWith(".js"))
+        catch (Exception ex)
         {
-            SetValues(values, "EmptyFiles", "BlankJS", string.Empty);
-            return true;
+            logger.LogError(ex, "Failed to deserialize session data.");
+            return null;
         }
-        if (route.EndsWith(".css"))
-        {
-            SetValues(values, "EmptyFiles", "BlankCss", string.Empty);
-            return true;
-        }
-        if (route.EndsWith(".png"))
-        {
-            SetValues(values, "EmptyFiles", "BlankPNG", string.Empty);
-            return true;
-        }
-        if (route.EndsWith(".jpg") || route.EndsWith(".jpeg"))
-        {
-            SetValues(values, "EmptyFiles", "BlankJPG", string.Empty);
-            return true;
-        }
-        if (route.EndsWith(".gif"))
-        {
-            SetValues(values, "EmptyFiles", "BlankGIF", string.Empty);
-            return true;
-        }
-        return false;
     }
 
-
-    /// <summary>
-    /// Handles the blog links in the route.
-    /// </summary>
-    /// <param name="route">The route value.</param>
-    /// <param name="values">The route values.</param>
-    /// <returns><c>true</c> if the blog link is handled; otherwise, <c>false</c>.</returns>
-    private bool HandleBlogLinks(string route, RouteValueDictionary values)
-    {
-        if (route == "/blog")
-        {
-            SetValues(values, "Blog", "index", string.Empty);
-            return true;
-        }
-        if (route.StartsWith("/posts/"))
-        {
-            var slug = route.Replace("/posts/", string.Empty);
-            SetValues(values, "Blog", "single", slug);
-            return true;
-        }
-        if (route.StartsWith("/categories/"))
-        {
-            var slug = route.Replace("/categories/", string.Empty);
-            SetValues(values, "Blog", "categories", slug);
-            return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// Handles the admin routes in the route.
-    /// </summary>
-    /// <param name="route">The route value.</param>
-    /// <param name="values">The route values.</param>
-    /// <returns><c>true</c> if the admin route is handled; otherwise, <c>false</c>.</returns>
     private bool HandleAdmin(string route, RouteValueDictionary values)
     {
         if (route.StartsWith("/admin/"))
         {
             var slug = route.Replace("/admin/", string.Empty);
-            SetValues(values, "admin", "index", slug);
+            SetValues(values, RouteConstants.Admin, "index", slug);
             return true;
         }
         return false;
     }
 
-    /// <summary>
-    /// Handles the base view route.
-    /// </summary>
-    /// <param name="httpContext">The current HttpContext.</param>
-    /// <param name="route">The route value.</param>
-    /// <param name="values">The route values.</param>
-    /// <returns>A task representing the asynchronous operation.</returns>
     private ValueTask<RouteValueDictionary> HandleBaseView(HttpContext httpContext, string route, RouteValueDictionary values)
     {
-        var value = httpContext?.Session.GetString("BaseViewKey");
-        var baseView = string.IsNullOrEmpty(value) ? null : JsonSerializer.Deserialize<WebsiteVM>(value);
+        var baseView = GetBaseView(httpContext);
         var myPage = baseView?.Menu?.FirstOrDefault(w => w.Url == route);
 
         if (myPage == null)
@@ -143,27 +59,130 @@ public class WebRouteValueTransformer(ILogger<WebRouteValueTransformer> logger) 
             }
             else
             {
-                logger.LogWarning("Page not found for route: {route}", route);
-                SetValues(values, "error", "notfound", route);
-                return new ValueTask<RouteValueDictionary>(values);
+                logger.LogWarning("Page not found for route: {Route}", route);
+                return CreateErrorRoute(values);
             }
         }
-
-        SetValues(values, myPage?.Controller ?? "Page", myPage?.Action ?? "index", myPage?.Argument ?? myPage?.Action);
+        SetValues(values, myPage?.Controller ?? "Page", myPage?.Action ?? "index", myPage?.Argument ?? myPage?.Action ?? string.Empty);
         return new ValueTask<RouteValueDictionary>(values);
     }
 
-    /// <summary>
-    /// Sets the controller, action, and id values in the route values.
-    /// </summary>
-    /// <param name="values">The route values.</param>
-    /// <param name="controller">The controller value.</param>
-    /// <param name="action">The action value.</param>
-    /// <param name="id">The id value.</param>
+    private bool HandleBlogLinks(string route, RouteValueDictionary values)
+    {
+        if (route.StartsWith("/posts/"))
+        {
+            var slug = route.Replace("/posts/", string.Empty);
+            SetValues(values, RouteConstants.Blog, "single", slug);
+            return true;
+        }
+        if (route.StartsWith("/categories/"))
+        {
+            var slug = route.Replace("/categories/", string.Empty);
+            SetValues(values, RouteConstants.Blog, "categories", slug);
+            return true;
+        }
+        return false;
+    }
+
+    private bool HandleFileExtensions(string route, RouteValueDictionary values)
+    {
+        var fileExtensions = new Dictionary<string, string>
+        {
+            { ".php", "BlankPHP" },
+            { ".js", "BlankJS" },
+            { ".css", "BlankCss" },
+            { ".png", "BlankPNG" },
+            { ".jpg", "BlankJPG" },
+            { ".jpeg", "BlankJPG" },
+            { ".gif", "BlankGIF" }
+        };
+
+        foreach (var (extension, action) in fileExtensions)
+        {
+            if (route.EndsWith(extension))
+            {
+                SetValues(values, RouteConstants.EmptyFiles, action, string.Empty);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool HandlePredefinedRoutes(string route, RouteValueDictionary values)
+    {
+        if (PredefinedRoutes.TryGetValue(route, out var mapping))
+        {
+            SetValues(values, mapping.Controller, mapping.Action, string.Empty);
+            return true;
+        }
+        return false;
+    }
+
     private static void SetValues(RouteValueDictionary values, string controller, string action, string id)
     {
         values["controller"] = controller;
         values["action"] = action;
         values["id"] = id;
+    }
+
+    /// <summary>
+    /// Transforms the route values asynchronously.
+    /// </summary>
+    /// <param name="httpContext">The current HttpContext.</param>
+    /// <param name="values">The route values.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    public override ValueTask<RouteValueDictionary> TransformAsync(HttpContext httpContext, RouteValueDictionary values)
+    {
+        string route = httpContext.Request.Path.Value ?? "/";
+        if (string.IsNullOrEmpty(route))
+        {
+            logger.LogError("Route is null or empty");
+            return CreateErrorRoute(values);
+        }
+
+        var baseView = GetBaseView(httpContext);
+        var myPage = baseView?.Menu?.FirstOrDefault(w => w.Url == route)
+            ?? baseView?.Menu?.FirstOrDefault(w => w.Argument?.ToLower() == route.Trim('/').ToLower());
+
+        if (myPage == null)
+        {
+            logger.LogWarning("Page not found for route: {Route}", route);
+            return CreateErrorRoute(values);
+        }
+
+
+        // Check if the argument matches but the route is different
+        if (myPage?.Url != route)
+        {
+            logger.LogInformation("Redirecting from route {Route} to {TargetUrl}", route, myPage?.Url);
+
+            // Perform the redirect
+            httpContext.Response.Clear(); // Clear any existing response data
+            httpContext.Response.StatusCode = StatusCodes.Status302Found; // Temporary redirect
+            httpContext.Response.Headers["Location"] = myPage.Url; // Set the new location header
+
+            // Flush the response and terminate
+            httpContext.Response.CompleteAsync();
+            return new ValueTask<RouteValueDictionary>([]);
+        }
+
+
+        if (HandlePredefinedRoutes(route, values)
+            || HandleFileExtensions(route, values)
+            || HandleBlogLinks(route, values)
+            || HandleAdmin(route, values))
+        {
+            return new ValueTask<RouteValueDictionary>(values);
+        }
+        return HandleBaseView(httpContext, route, values);
+    }
+
+    private static class RouteConstants
+    {
+        public const string Admin = "admin";
+        public const string BaseViewSessionKey = "BaseViewKey";
+        public const string Blog = "Blog";
+        public const string EmptyFiles = "EmptyFiles";
+        public const string Error = "error";
     }
 }
