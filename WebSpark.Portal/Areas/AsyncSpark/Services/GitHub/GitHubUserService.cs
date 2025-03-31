9ad712b434d148f6b18cb64c2f114c45d75bbe37
Mark Hazleton <mark.hazleton@controlorigins.com>
2025-03-31 06:54:46 -0500

    updates for editing

--- WebSpark.Portal/Areas/AsyncSpark/Services/GitHub/GitHubUserService.cs
+++ WebSpark.Portal/Areas/AsyncSpark/Services/GitHub/GitHubUserService.cs
@@ -84,7 +84,7 @@ public class GitHubUserService
         {
             AddLookupLimitError(fetchedData);
         }
-        _memoryCacheManager.Set(_userLookupListCacheKey, userList, 60);
+        _memoryCacheManager.Set(_userLookupListCacheKey, userList, 360);
         return fetchedData;
     }
 }
--- WebSpark.Portal/Areas/WebCMS/Controllers/HomeController.cs
+++ WebSpark.Portal/Areas/WebCMS/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using WebSpark.Core.Models;
 namespace WebSpark.Portal.Areas.WebCMS.Controllers
 {
     /// <summary>
-    /// Home controller for the WebCMS area - provides dashboard and overview functionality
+    /// Home controller for the WebSpark CMS area - provides dashboard and overview functionality
     /// </summary>
     public class HomeController : WebCMSBaseController
     {
@@ -68,14 +68,14 @@ namespace WebSpark.Portal.Areas.WebCMS.Controllers
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error loading WebCMS dashboard");
+                _logger.LogError(ex, "Error loading WebSpark CMS dashboard");
                 TempData["ErrorMessage"] = "An error occurred while loading the dashboard.";
                 return View();
             }
         }
 
         /// <summary>
-        /// Displays help/documentation for the WebCMS system
+        /// Displays help/documentation for the WebSpark CMS system
         /// </summary>
         [HttpGet]
         public IActionResult Help()
