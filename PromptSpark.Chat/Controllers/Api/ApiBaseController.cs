d13612755dda29dea3adc4eb0bdb094cff0e260d
Mark Hazleton <mark.hazleton@controlorigins.com>
2025-04-28 07:12:26 -0500

    update pacakages, conver to npm for js/css libraries

--- PromptSpark.Chat/Controllers/Api/ApiBaseController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace PromptSpark.Chat.Controllers.Api;
-
-[Route("api/[controller]")]
-[ApiController]
-public class ApiBaseController : ControllerBase
-{
-}
